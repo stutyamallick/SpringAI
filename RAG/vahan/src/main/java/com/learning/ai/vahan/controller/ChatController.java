07026bcad3377640e696ad2fbc91e7916ee11193
Stutya Mallick <stutya.mallick@gmail.com>
2025-09-04 16:35:45 +0530

    [RAG Pattern] No Error But NOT working

---
+++ RAG/vahan/src/main/java/com/learning/ai/vahan/controller/ChatController.java
@@ -0,0 +1,78 @@
+package com.learning.ai.vahan.controller;
+
+import com.learning.ai.vahan.entity.Cars;
+import com.learning.ai.vahan.repository.CarsRepository;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+    private final CarsRepository carsRepository;
+
+    @Value("classpath:/prompts/rag-prompt.st")
+    private Resource ragPromptTemplate;
+
+    public ChatController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore,
+                          CarsRepository carsRepository) {
+        this.carsRepository = carsRepository;
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .build();
+        this.vectorStore = vectorStore;
+    }
+
+    @GetMapping("/api/chat")
+    public String chat(@RequestParam String query){
+        List<Cars> cars = carsRepository.findAll();
+
+        List<Document> documentList = new ArrayList<>();
+        for(Cars car: cars) {
+            Document document = new Document(car.toString());
+            System.out.println(document);
+            documentList.add(document);
+        }
+        vectorStore.add(documentList);
+
+        List<Document> similarDocuments = vectorStore
+                .similaritySearch(SearchRequest
+                        .builder()
+                        .query(query)
+                        .topK(4)
+                        .build());
+
+        System.out.println(">>> Similar documents: " + similarDocuments);
+
+        String information = similarDocuments.stream()
+                .map(Document::getText)
+                .collect(Collectors.joining("\n"));
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(ragPromptTemplate);
+        Prompt prompt = new Prompt(List.of(
+                systemPromptTemplate.createMessage(
+                        Map.of("information", information)),
+                new UserMessage(query)));
+
+        System.out.println(">>> Prompt: " + prompt.getContents());
+
+        return chatClient.prompt(prompt).call().content();
+    }
+}
--- RAG/vahan/src/main/java/com/learning/ai/vahan/entity/Cars.java
+++ RAG/vahan/src/main/java/com/learning/ai/vahan/entity/Cars.java
@@ -1,29 +1,47 @@
 package com.learning.ai.vahan.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Cars {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "brand")
     private String brand;
+
+    @Column(name = "model")
     private String model;
+
+    @Column(name = "model_year")
     private Integer modelYear;
+
+    @Column(name = "transmission_type")
     private String transmissionType;
+
+    @Column(name = "fuel_type")
     private String fuelType;
+
+    @Column(name = "body_type")
     private String bodyType;
+
+    @Column(name = "siting_capacity")
     private Integer sitingCapacity;
+
+    @Column(name = "km_driven")
     private Integer kmDriven;
+
+    @Column(name = "color")
     private String color;
+
+    @Column(name = "price")
     private Float price;
 }
